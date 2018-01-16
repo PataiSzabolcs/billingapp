341d2cbead5ec65979715b2283cbed72c09ba527
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-16 16:40:33 +0100

    feat(billing app): change to postgre sql with connectionstring

--- MyBillingApp/Startup.cs
+++ MyBillingApp/Startup.cs
@@ -10,18 +10,30 @@ using Microsoft.Extensions.DependencyInjection;
 using MyBillingApp.Models;
 using MyBillingApp.Entities;
 using MyBillingApp.Repositories;
+using Microsoft.Extensions.Configuration;
 
 namespace MyBillingApp
 {
     public class Startup
     {
+
+        public static IConfigurationRoot Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddScoped<ClientRepository>();
-            services.AddDbContext<ClientContext>(options => options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyBillingApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+            services.AddDbContext<ClientContext>(options => options.UseNpgsql(Configuration["CONNECTIONSTRING"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
